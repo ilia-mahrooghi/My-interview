65857392e7b1556826f2afca306e6c8bf62b0b19
llya mahrooghi <ilyamahrooghimath@gmail.com>
2021-12-11 00:48:02 +0330

    salam

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+ function func(){
+             document.getElementById('func').style="text-align: center;"
+         }
+ function submit(){
+             var x=confirm("ایا از ارسال خود اطمینان دارید؟");
+              if (x== true){alert('باموفقیت انجام شد');}
+ }
+
